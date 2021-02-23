66eab5497e7d2862158394dc7b8feb5728113476
zacharyjhumphrey <zacharyjhumphrey@gmail.com>
2021-02-23 15:51:52 -0600

    Trying to serve assets differently

--- Server/app.js
+++ Server/app.js
@@ -14,7 +14,8 @@ http.listen(process.env.PORT || 3000);
 // SETTING UP PAGES
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
-app.use(express.static('./server/assets'));
+// app.use(express.static('./server/assets'));
+app.use(express.static(path.join(process.cwd(), 'server/assets')));
 
 // BODYPARSER
 app.use(express.urlencoded({ extended: true }));
